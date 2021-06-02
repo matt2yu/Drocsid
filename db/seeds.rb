e3a6e929a6bcafadc68c8986b5102b410a8089e5
matt2yu <matt2yu@gmail.com>
2021-06-02 12:16:29 -0500

    Started messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# User.destroy_all
+# users = []
+# 99.times {
+#   users << User.create!(
+#     email:
+#   )
+# }
