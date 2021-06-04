b70017363c1568a9ddc14be514d4caa9c1de3247
matt2yu <matt2yu@gmail.com>
2021-06-04 00:57:13 -0500

    Heroku push

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,5 +1,5 @@
 import * as APIUtil from '../util/session_api_util';
-
+// test
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
 export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
