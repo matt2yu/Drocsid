e6c017b4e8d83e84d84b91760c07120322de03cc
matt2yu <matt2yu@gmail.com>
2021-07-02 18:52:19 -0500

    format

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,11 +1,11 @@
 class Message < ApplicationRecord
   validates :body, :author_id, :messagable_id, :messagable_type, presence: true
-
+  
+  belongs_to :messagable,
+    polymorphic: true
   belongs_to :user,
     primary_key: :id,
     foreign_key: :author_id,
     class_name: :User
   
-  belongs_to :messagable,
-    polymorphic: true
 end
